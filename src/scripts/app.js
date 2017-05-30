dab346b12fefa0476c3ffa7296dd3e52137c989d
Kurt Schneider <k.w.schneid@gmail.com>
2017-05-30 18:04:58 -0400

    added some functionality anand new section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -16,9 +16,9 @@ $(function() {
   });
 });
 
-  $(".piece").on('click', function(){
-    $(this).toggleClass('fullscreen');
-  })
+$(".piece").on('click', function(){
+  $(this).toggleClass('fullscreen');
+})
 
 // ABOUT MODAL
 
@@ -35,19 +35,59 @@ $('.close').click(function(){
 // When user clicks outside of modal close modal
 
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == about) {
-//     about.style.display = "none";
-//   }
-// }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == about) {
+    about.style.display = "none";
+  }
+}
 
 });
 
 
 
+
+
+
+// $(function() {
+//   $(".img-w").each(function() {
+//     $(this).wrap("<div class='img-c'></div>")
+//     let imgSrc = $(this).find("img").attr("src");
+//      $(this).css('background-image', 'url(' + imgSrc + ')');
+//   })
+            
+  
+//   $(".img-c").click(function() {
+//     let w = $(this).outerWidth()
+//     let h = $(this).outerHeight()
+//     let x = $(this).offset().left
+//     let y = $(this).offset().top
+    
+    
+//     $(".active").not($(this)).remove()
+//     let copy = $(this).clone();
+//     copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
+//     $(".active").css('top', y - 8);
+//     $(".active").css('left', x - 8);
+    
+//       setTimeout(function() {
+//     copy.addClass("positioned")
+//   }, 0)
+    
+//   }) 
+  
+  
+// })
+
+// $(document).on("click", ".img-c.active", function() {
+//   let copy = $(this)
+//   copy.removeClass("positioned active").addClass("postactive")
+//   setTimeout(function() {
+//     copy.remove();
+//   }, 500)
+// })
